b25ed106ce714b610f282670c5fbc7185864b319
devintheamateurdevin <devintheamateurdevin@gmail.com>
2023-12-30 22:44:34 +0300

    exports more in main

--- build/src/main.js
+++ build/src/main.js
@@ -1,3 +1,5 @@
 export * from "../specs/api-schema.js";
 export * from "../src/parsers/parsers.js";
+export * from "../src/parsers/SpaceBasedWordParser.js";
+export * from "../src/parsers/WordParser.js";
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,4 @@
 export * from "@/specs/api-schema.js";
 export * from "@/src/parsers/parsers.js";
+export * from "@/src/parsers/SpaceBasedWordParser.js";
+export * from "@/src/parsers/WordParser.js";
